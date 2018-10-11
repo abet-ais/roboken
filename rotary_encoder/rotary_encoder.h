dbee8c116e4ae8ba06a5f998b0d29f1302b194a8
taiki <15t1004j@shinshu-u.ac.jp>
2018-10-11 23:48:47 +0900

    correct encoder.h

--- rotary_encoder/rotary_encoder.h
+++ rotary_encoder/rotary_encoder.h
@@ -1,116 +1,51 @@
-#include "rotary_encoder.h"
+#ifndef _rotary_encoder_H_
+#define _rotary_encoder_H_
 
-rotary_encoder::rotary_encoder(
-         PinName channelA,
-         PinName channelB,
-         PinName index,
-         int pulsesPerRev,
-         Encoding encoding) : channelA_(channelA), channelB_(channelB),
-        index_(index) {
-
-    pulses_       = 0;
-    revolutions_  = 0;
-    pulsesPerRev_ = pulsesPerRev;
-    encoding_     = encoding;
-    deg           = 0.0;
-    rad           = 0.0;
-
-    int chanA = channelA_.read();
-    int chanB = channelB_.read();
-
-    currState_ = (chanA << 1) | (chanB);
-    prevState_ = currState_;
-
-    channelA_.rise(this, &rotary_encoder::encode);
-    channelA_.fall(this, &rotary_encoder::encode);
-
-    if (encoding == X4_ENCODING) {
-        channelB_.rise(this, &rotary_encoder::encode);
-        channelB_.fall(this, &rotary_encoder::encode);
-        magnification=4;
-    }
-    else{
-        magnification=2;
-    }
-
-    if (index !=  NC) {
-        index_.rise(this, &rotary_encoder::index);
-    }
-
-}
-
-void rotary_encoder::reset(void) {
-
-    pulses_      = 0;
-    revolutions_ = 0;
-    deg          = 0.0;
-    rad          = 0.0;
-
-}
-
-int rotary_encoder::getCurrentState(void) {
-
-    return currState_;
-
-}
-
-int rotary_encoder::getPulses(void) {
-
-    return pulses_;
-
-}
-
-float rotary_encoder::getDeg(void) {
-    float resolution_deg = (360.0/((float)pulsesPerRev_*magnification));//分解能
-    
-    deg = pulses_*resolution_deg;
-    
-    return deg;
-
-}
-
-float rotary_encoder::getRad(void){
-    float resolution_rad = (2*3.14159/((float)pulsesPerRev_*magnification));//分解能
-    
-    rad = pulses_*resolution_rad;
-    
-    return rad;
-}
-
-    
-
-void rotary_encoder::encode(void) {
-
-    int change = 0;
-    int chanA  = channelA_.read();
-    int chanB  = channelB_.read();
-
-    currState_ = (chanA << 1) | (chanB);
-
-    if (encoding_ == X2_ENCODING) {
-        if ((prevState_ == 0x3 && currState_ == 0x0) ||
-                (prevState_ == 0x0 && currState_ == 0x3)) {
-            pulses_++;
-        }
-        else if ((prevState_ == 0x2 && currState_ == 0x1) ||
-                 (prevState_ == 0x1 && currState_ == 0x2)) {
-            pulses_--;
-        }
-    } else if (encoding_ == X4_ENCODING) {
-        if (((currState_ ^ prevState_) != INVALID) && (currState_ != prevState_)) {
-            change = (prevState_ & PREV_MASK) ^ ((currState_ & CURR_MASK) >> 1);
-            if (change == 0) {
-                change = -1;
-            }
-           pulses_ -= change;
-        }
-    }
-    prevState_ = currState_;
-}
+#include "mbed.h"
 
 
-void rotary_encoder::index(void) {
+#define PREV_MASK 0x1 
+#define CURR_MASK 0x2 
+#define INVALID   0x3 
+class rotary_encoder {
 
-    revolutions_++;
+public:
 
-}
+    typedef enum Encoding {
+
+        X2_ENCODING,
+        X4_ENCODING
+
+    } Encoding;
+
+
+    rotary_encoder(PinName channelA, PinName channelB, PinName index, int pulsesPerRev, Encoding encoding = X2_ENCODING);
+
+    void reset(void);
+    int getCurrentState(void);
+    int getPulses(void);
+    float getDeg(void);
+    float getRad(void);
+
+private:
+
+
+    void encode(void);
+    void index(void);
+    Encoding encoding_;
+    InterruptIn channelA_;
+    InterruptIn channelB_;
+    InterruptIn index_;
+
+    int          pulsesPerRev_;
+    int          prevState_;
+    int          currState_;
+    volatile int pulses_;
+    volatile int revolutions_;
+    int          magnification; //角度の分解能を決めるために必要な変数
+    float        deg;
+    float        rad;
+
+};
+
+#endif 
